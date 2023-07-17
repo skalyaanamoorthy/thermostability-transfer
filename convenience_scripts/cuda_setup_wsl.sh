ae96d753af4ba4a59d31c1283d92d45619169175
Subha Kalyaanamoorthy <64178961+skalyaanamoorthy@users.noreply.github.com>
2023-07-17 13:59:26 -0400

    Update cuda_setup_wsl.sh

--- convenience_scripts/cuda_setup_wsl.sh
+++ convenience_scripts/cuda_setup_wsl.sh
@@ -1,3 +1,5 @@
+sudo apt-get update
+sudo apt-get upgrade
 wget https://developer.download.nvidia.com/compute/cuda/repos/wsl-ubuntu/x86_64/cuda-wsl-ubuntu.pin
 sudo mv cuda-wsl-ubuntu.pin /etc/apt/preferences.d/cuda-repository-pin-600
 wget https://developer.download.nvidia.com/compute/cuda/11.8.0/local_installers/cuda-repo-wsl-ubuntu-11-8-local_11.8.0-1_amd64.deb
